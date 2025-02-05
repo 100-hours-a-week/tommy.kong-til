9c435f5a531c7a9b58d5b264b7af6ee83162f183
tykong08 <taeyoung.kong@outlook.com>
2025-02-05 10:19:39 +0900

    2주차_miniquest

--- Feb/2주차_miniquest.py
+++ Feb/2주차_miniquest.py
@@ -195,3 +195,159 @@ data = {
 df = pd.DataFrame(data)
 print(df.groupby("부서").filter(lambda x: x["급여"].mean() > 5000))
 print()
+# 병합
+# 1.
+df1 = pd.DataFrame({'고객ID': [1, 2, 3], '이름': ['홍길동', '김철수', '이영희']})
+df2 = pd.DataFrame({'고객ID': [2, 3, 4], '구매액': [10000, 20000, 30000]})
+df3 = pd.merge(df1,df2,on='고객ID',how='inner')
+print(df3)
+print()
+
+# 2. 
+df1 = pd.DataFrame({'고객ID': [1, 2, 3], '이름': ['홍길동', '김철수', '이영희']})
+df2 = pd.DataFrame({'고객ID': [2, 3, 4], '구매액': [15000, 25000, 35000]})
+df3 = pd.merge(df1,df2,on='고객ID',how='left')
+print(df3)
+print()
+# 3.
+df1 = pd.DataFrame({
+    '고객ID': [1, 2, 3],
+    '도시': ['서울', '부산', '대전'],
+    '구매액': [10000, 20000, 30000]
+})
+
+df2 = pd.DataFrame({
+    '고객ID': [1, 2, 3],
+    '도시': ['서울', '부산', '광주'],
+    '구매액': [15000, 25000, 35000]
+})
+
+df3 = pd.merge(df1,df2,on=['고객ID','도시'],suffixes=['_1','_2'])
+
+print(df3)
+print()
+
+# 결측치 처리
+## 1.
+data = {'이름': ['홍길동', '김철수', np.nan, '이영희'],
+        '나이': [25, np.nan, 30, 28],
+        '성별': ['남', '남', '여', np.nan]}
+
+df = pd.DataFrame(data)
+print(df.isna().sum())
+print()
+
+## 2.
+data = {'이름': ['홍길동', '김철수', np.nan, '이영희'],
+        '나이': [25, np.nan, 30, 28],
+        '성별': ['남', '남', '여', np.nan]}
+
+df = pd.DataFrame(data)
+print(df.dropna())
+print()
+
+## 3.
+data = {'이름': ['홍길동', '김철수', np.nan, '이영희'],
+        '나이': [25, np.nan, 30, 28],
+        '성별': ['남', '남', '여', np.nan]}
+
+df = pd.DataFrame(data)
+df['나이'] = df['나이'].fillna(df['나이'].mean()).astype('int')
+print(df)
+print()
+
+# 피벗
+## 1. 
+data = {
+    '날짜': ['2024-01-01', '2024-01-01', '2024-01-02', '2024-01-02'],
+    '제품': ['A', 'B', 'A', 'B'],
+    '판매량': [100, 200, 150, 250]
+}
+
+df = pd.DataFrame(data)
+
+df_new = df.pivot(index='날짜',columns='제품',values='판매량')
+print(df_new)
+print()
+
+#@ 2.
+data = {
+    '카테고리': ['전자', '가전', '전자', '가전'],
+    '제품': ['A', 'B', 'A', 'B'],
+    '판매량': [100, 200, 150, 250]
+}
+
+df = pd.DataFrame(data)
+print(df.pivot_table(index='카테고리',columns='제품',values='판매량',aggfunc='sum'))
+print()
+
+## 3.
+data = {
+    '날짜': ['2024-01-01', '2024-01-01', '2024-01-02', '2024-01-02'],
+    '제품': ['A', 'B', 'A', 'B'],
+    '판매량': [100, 200, 150, 250],
+    '이익': [20, 50, 30, 60]
+}
+
+df = pd.DataFrame(data)
+
+print(df.pivot(index='날짜',columns='제품',values=['판매량','이익']))
+print()
+
+# 중복 제거
+## 1.
+data = {
+    '이름': ['김철수', '이영희', '김철수', '박민수'],
+    '나이': [25, 30, 25, 40],
+    '성별': ['남', '여', '남', '남']
+}
+
+df = pd.DataFrame(data)
+
+print(df.drop_duplicates())
+print()
+
+## 2.
+data = {
+    '제품': ['노트북', '태블릿', '노트북', '스마트폰'],
+    '가격': [1500000, 800000, 1500000, 1000000],
+    '카테고리': ['전자기기', '전자기기', '전자기기', '전자기기']
+}
+
+df = pd.DataFrame(data)
+
+df.drop_duplicates(subset=['제품'],inplace=True)
+print('없음' if df.duplicated().sum()  == 0 else '있음')
+print()
+
+## 3.
+data = {
+    '학생': ['김민수', '박지현', '김민수', '이정훈'],
+    '성적': [90, 85, 90, 88],
+    '학교': ['A고', 'B고', 'A고', 'C고']
+}
+
+df = pd.DataFrame(data)
+df.drop_duplicates(inplace=True)
+print(df)
+print()
+
+# 문자열 처리 
+
+## 1.
+data = pd.Series(["HELLO", "WORLD", "PYTHON", "PANDAS"])
+print(data.str.lower())
+print()
+
+## 2.
+df = pd.DataFrame({"이름": [" John Doe ", "Alice ", " Bob", "Charlie Doe "]})
+print(df['이름'][df['이름'].str.strip().str.contains('Doe')])
+print()
+
+## 3.
+df = pd.DataFrame({"설명": ["빅데이터 분석", "데이터 과학", "머신 러닝", "딥 러닝"]})
+def abbre(x):
+    sp = x.split(' ')
+    return sp[0][0] + sp[1][0]
+df['약어'] = df['설명'].apply(abbre)
+print(df['약어'])
