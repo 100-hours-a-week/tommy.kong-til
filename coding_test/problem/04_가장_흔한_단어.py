562fb97f77d242d3e28b347393b240366704d40c
tykong08 <taeyoung.kong@outlook.com>
2025-04-08 09:46:21 +0900

    vault backup: 2025-04-08 09:46:21

--- coding_test/problem/04_가장_흔한_단어.py
+++ coding_test/problem/04_가장_흔한_단어.py
@@ -2,6 +2,9 @@
 
 # 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라
 
+from typing import List
+
+
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        for i in range(len(paragraph)):
+        p_list = paragraph.split()
